3eb8f14c2689412499dee6e37c70a2d00a764376
Ross Cairns <ross@electricglen.com>
2013-01-20 19:46:19 +0000

    Fix for data path

--- src/AppDelegate.mm
+++ src/AppDelegate.mm
@@ -1,8 +1,12 @@
 #import "AppDelegate.h"
+#import "ofMain.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // set the root application path so we can load resources
+    ofSetDataPathRoot( [[NSString stringWithFormat:@"%@/", [[NSBundle mainBundle] resourcePath]] cStringUsingEncoding:NSUTF8StringEncoding] );
     
     return YES;
 }
