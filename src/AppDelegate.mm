f9023ed2d097f4aa2d08f6c45dea8c1d17f3eb96
Ross Cairns <ross@electricglen.com>
2013-01-21 11:29:44 +0000

    Fix for rotation events and ofDataPath
    [super applicationDidFinishLaunching] can not be called, but is recreated.

--- src/AppDelegate.mm
+++ src/AppDelegate.mm
@@ -5,8 +5,36 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // set the root application path so we can load resources
-    ofSetDataPathRoot( [[NSString stringWithFormat:@"%@/", [[NSBundle mainBundle] resourcePath]] cStringUsingEncoding:NSUTF8StringEncoding] );
+    // We can't do this:
+    // [super applicationDidFinishLaunching:application];
+    //
+    // as it will report not having a view controller
+    // as it creates a new UIWindow so we need to do the following:
+    
+    // set the root application path
+    ofSetDataPathRoot([[NSString stringWithFormat:@"%@/", [[NSBundle mainBundle] resourcePath]] cStringUsingEncoding:NSUTF8StringEncoding]);
+    
+    // Listen to events
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    
+    NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+    
+    [center addObserver:self
+               selector:@selector(receivedRotate:)
+                   name:UIDeviceOrientationDidChangeNotification
+                 object:nil];
+    
+    [center addObserver:self
+               selector:@selector(handleScreenConnectNotification:)
+                   name:UIScreenDidConnectNotification object:nil];
+    
+    [center addObserver:self
+               selector:@selector(handleScreenDisconnectNotification:)
+                   name:UIScreenDidDisconnectNotification object:nil];
+    
+    [center addObserver:self
+               selector:@selector(handleScreenModeDidChangeNotification:)
+                   name:UIScreenDidDisconnectNotification object:nil];
     
     return YES;
 }
